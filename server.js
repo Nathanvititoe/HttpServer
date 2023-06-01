e614e3598be37ceb4e7068b2eecd4464b5457bb4
Nathanvititoe <nathanvititoe@gmail.com>
2023-06-01 14:04:45 -0700

    first http server

---
+++ server.js
@@ -0,0 +1,50 @@
+// 'use strict';
+
+// var http = require('http');
+// var port = process.env.PORT || 8000;
+
+// var server = http.createServer(function(req, res) {
+//   var guests = ['Mary', 'Don'];
+//   var guestsJSON = JSON.stringify(guests);
+
+//   res.setHeader('Content-Type', 'application/json');
+//   res.end(guestsJSON);
+// });
+
+// server.listen(port, function() {
+//   console.log('Listening on port', port);
+// });
+
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var guestsPath = path.join(__dirname, 'guests.json');
+
+var http = require('http');
+var port = process.env.PORT || 8000;
+
+var server = http.createServer(function(req, res) {
+  if (req.method === 'GET' && req.url === '/guests') {
+    fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
+        if(err) {
+            console.error(err.stack);
+            res.statusCode = 500;
+            res.setHeader('Content-type', 'text/plain');
+            return res.end('Internal Server error');
+        
+        }
+        res.setHeader('Content-Type', 'application/json')
+        res.end(guestsJSON)
+    });
+}  
+  else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not found');
+  }
+});
+
+server.listen(port, function() {
+  console.log('Listening on port', port);
+});
